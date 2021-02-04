2b81e78173ee4bdefe0f52d2d27d34dec55ae042
Victorique <loli@victorique.moe>
2021-02-04 03:07:08 +0000

    added typing to method

--- src/commands/status.ts
+++ src/commands/status.ts
@@ -3,8 +3,8 @@ import {Command, CommandMessage} from "@typeit/discord";
 export abstract class Status {
     private isEnabled = false;
 
-    @Command("ping")
-    public ping(command: CommandMessage): void {
+    @Command("status")
+    public status(command: CommandMessage): void {
         console.log(`user: ${command.author.username} Enquired about the toy status`);
         command.channel.send(this.isEnabled ? "My vibrator is enabled!" : "My vibrator is disabled!");
     }
