7619507b243ff22eb0864b41d251192635292d67
VictoriqueMoe <loli@victorique.moe>
2021-02-22 02:05:55 +0000

    added finer kick and ban logs

--- src/events/closeableModules/AuditLogger.ts
+++ src/events/closeableModules/AuditLogger.ts
@@ -3,8 +3,16 @@ import {ArgsOf, Client, Guard, On} from "@typeit/discord";
 import {EnabledGuard} from "../../guards/EnabledGuard";
 import {CloseableModule} from "../../model/CloseableModule";
 import {DiscordUtils} from "../../utils/Utils";
+import {User} from "discord.js";
 
-export class AuditLogger extends CloseableModule{
+/**
+ * Non admin audit Logger for quick logs. this will log:
+ * Member join
+ * Member ban
+ * Member kick
+ * Member leave
+ */
+export class AuditLogger extends CloseableModule {
     constructor() {
         super(CloseOptionModel);
     }
@@ -20,6 +28,36 @@ export class AuditLogger extends CloseableModule{
     @Guard(EnabledGuard("userLog"))
     private async memberLeaves([member]: ArgsOf<"guildMemberAdd">, client: Client): Promise<void> {
         const memberLeft = member.id;
+        const banLogs = await member.guild.fetchAuditLogs({
+            limit: 1,
+            type: 'MEMBER_BAN_ADD',
+        });
+        const banLog = banLogs.entries.first();
+        if (banLog) {
+            const target = banLog.target;
+            if (target instanceof User) {
+                if (target.id === memberLeft) {
+                    return;
+                }
+            }
+        }
+
+        const kickLogs = await member.guild.fetchAuditLogs({
+            limit: 1,
+            type: 'MEMBER_KICK',
+        });
+        const kickLog = kickLogs.entries.first();
+        if (kickLog) {
+            const target = kickLog.target;
+            if (target instanceof User) {
+                if (target.id === memberLeft) {
+                    const personWhoDidKick = banLog.executor;
+                    const reason = banLog.reason;
+                    DiscordUtils.postToLog(`<@${memberLeft}> has been kicked by ${personWhoDidKick.username} for reason: "${reason}"`);
+                    return;
+                }
+            }
+        }
         DiscordUtils.postToLog(`<@${memberLeft}> has left the server`);
     }
 
@@ -27,7 +65,16 @@ export class AuditLogger extends CloseableModule{
     @Guard(EnabledGuard("userLog"))
     private async memberBanned([guild, user]: ArgsOf<"guildBanAdd">, client: Client): Promise<void> {
         const memberBanned = user.id;
-        DiscordUtils.postToLog(`<@${memberBanned}> has been banned!!`);
+        const fetchedLogs = await guild.fetchAuditLogs({
+            limit: 1,
+            type: 'MEMBER_BAN_ADD',
+        });
+        const banLog = fetchedLogs.entries.first();
+        if (banLog) {
+            const personWhoDidBan = banLog.executor;
+            const reason = banLog.reason;
+            DiscordUtils.postToLog(`<@${memberBanned}> has been BANNED by ${personWhoDidBan.username} for reason: "${reason}"`);
+        }
     }
 
 
