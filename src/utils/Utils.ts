0ef352ffc9c202d4290b8730f7c5397f557d9a21
VictoriqueMoe <loli@victorique.moe>
2021-10-05 15:04:36 +0100

    added deep compare

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -688,6 +688,93 @@ export class ObjectUtil {
         return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
     }
 
+    public static deepCompare(...objects: Record<string, any>[]): boolean {
+        let i, l, leftChain, rightChain;
+
+        function compare2Objects(x: any, y: any): boolean {
+            let p;
+            if (isNaN(x) && isNaN(y) && typeof x === 'number' && typeof y === 'number') {
+                return true;
+            }
+            if (x === y) {
+                return true;
+            }
+            if ((typeof x === 'function' && typeof y === 'function') ||
+                (x instanceof Date && y instanceof Date) ||
+                (x instanceof RegExp && y instanceof RegExp) ||
+                (x instanceof String && y instanceof String) ||
+                (x instanceof Number && y instanceof Number)) {
+                return x.toString() === y.toString();
+            }
+            if (!(x instanceof Object && y instanceof Object)) {
+                return false;
+            }
+
+            if (x.isPrototypeOf(y) || y.isPrototypeOf(x)) {
+                return false;
+            }
+
+            if (x.constructor !== y.constructor) {
+                return false;
+            }
+
+            if (x.prototype !== y.prototype) {
+                return false;
+            }
+
+            if (leftChain.indexOf(x) > -1 || rightChain.indexOf(y) > -1) {
+                return false;
+            }
+            for (p in y) {
+                if (y.hasOwnProperty(p) !== x.hasOwnProperty(p)) {
+                    return false;
+                } else if (typeof y[p] !== typeof x[p]) {
+                    return false;
+                }
+            }
+            for (p in x) {
+                if (y.hasOwnProperty(p) !== x.hasOwnProperty(p)) {
+                    return false;
+                } else if (typeof y[p] !== typeof x[p]) {
+                    return false;
+                }
+
+                switch (typeof (x[p])) {
+                    case 'object':
+                    case 'function':
+                        leftChain.push(x);
+                        rightChain.push(y);
+                        if (!compare2Objects(x[p], y[p])) {
+                            return false;
+                        }
+                        leftChain.pop();
+                        rightChain.pop();
+                        break;
+                    default:
+                        if (x[p] !== y[p]) {
+                            return false;
+                        }
+                        break;
+                }
+            }
+
+            return true;
+        }
+
+        if (arguments.length < 1) {
+            return true;
+        }
+
+        for (i = 1, l = objects.length; i < l; i++) {
+            leftChain = [];
+            rightChain = [];
+            if (!compare2Objects(objects[0], objects[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static getAmountOfCapsAsPercentage(valueCheck: string): number {
         if (!ObjectUtil.validString(valueCheck)) {
             return 0;
