7a24854f1cd3437619f9881c60678960c072e1a4
VictoriqueMoe <loli@victorique.moe>
2021-10-10 18:25:12 +0100

    downgrade eslint until typescript-eslint is updated

--- src/events/closeableModules/logging/admin/MessageLogger.ts
+++ src/events/closeableModules/logging/admin/MessageLogger.ts
@@ -59,7 +59,7 @@ export class MessageLogger extends AbstractAdminAuditLogger {
 
     @On("messageDelete")
     private async messageDeleted([message]: ArgsOf<"messageDelete">, client: Client): Promise<void> {
-        if (!Main.testMode && message.member && message.member.id === client.user.id) {
+        if (!Main.testMode && message?.member?.id === client.user.id) {
             return;
         }
         const dateNow = Date.now();
