fe251710d6a58d7b4d502ee1205db97532d4c460
VictoriqueMoe <loli@victorique.moe>
2021-11-08 10:26:35 +0000

    fixed db marsaller

--- src/model/DB/AbstractEventSecurityConstraint.ts
+++ src/model/DB/AbstractEventSecurityConstraint.ts
@@ -8,37 +8,7 @@ import {Client} from "discordx";
 
 export abstract class AbstractEventSecurityConstraint extends AbstractModel implements IEventSecurityConstraint {
 
-    private static getRoles(this: AbstractEventSecurityConstraint, prop: string): void {
-        const value: string | null = this[prop];
-        if (!ObjectUtil.validString(value)) {
-            this[prop] = [];
-            return;
-        }
-        const guild = AbstractEventSecurityConstraint.getGuild.call(this);
-        const roleIds = value.split(",");
-        this[prop] = roleIds.map(roleId => guild.roles.cache.get(roleId));
-    }
-
-    private static getChannels(this: AbstractEventSecurityConstraint, prop: string): void {
-        const value: string | null = this[prop];
-        if (!ObjectUtil.validString(value)) {
-            this[prop] = [];
-            return;
-        }
-        const guild = AbstractEventSecurityConstraint.getGuild.call(this);
-        const channels = value.split(",");
-        this[prop] = channels.map(channelId => guild.channels.cache.get(channelId));
-    }
-
-    private static setChannels(this: AbstractEventSecurityConstraint, channels: GuildChannel[], prop: string): void {
-        if (!ArrayUtils.isValidArray(channels)) {
-            this[prop] = null;
-            return;
-        }
-        this[prop] = channels.map(channel => channel.id);
-    }
-
-    private static setRoles(this: AbstractEventSecurityConstraint, roles: GuildChannel[], prop: string): void {
+    private setRoles(roles: Role[], prop: string): void {
         if (!ArrayUtils.isValidArray(roles)) {
             this[prop] = null;
             return;
@@ -46,7 +16,37 @@ export abstract class AbstractEventSecurityConstraint extends AbstractModel impl
         this[prop] = roles.map(role => role.id);
     }
 
-    private static getGuild(this: AbstractEventSecurityConstraint): Guild {
+    private getRoles(prop: string): void {
+        const value: string | null = this[prop];
+        if (!ObjectUtil.validString(value)) {
+            this[prop] = [];
+            return;
+        }
+        const guild = this.getGuild();
+        const roleIds = value.split(",");
+        this[prop] = roleIds.map(roleId => guild.roles.cache.get(roleId));
+    }
+
+    private getChannels(prop: string): void {
+        const value: string | null = this[prop];
+        if (!ObjectUtil.validString(value)) {
+            this[prop] = [];
+            return;
+        }
+        const guild = this.getGuild();
+        const channels = value.split(",");
+        this[prop] = channels.map(channelId => guild.channels.cache.get(channelId));
+    }
+
+    private setChannels(channels: GuildChannel[], prop: string): void {
+        if (!ArrayUtils.isValidArray(channels)) {
+            this[prop] = null;
+            return;
+        }
+        this[prop] = channels.map(channel => channel.id);
+    }
+
+    private getGuild(): Guild {
         const client = container.resolve(Client);
         return client.guilds.cache.get(this.guildId);
     }
@@ -57,11 +57,11 @@ export abstract class AbstractEventSecurityConstraint extends AbstractModel impl
      */
     @AfterLoad()
     private unMarshallTransformer(): void {
-        AbstractEventSecurityConstraint.getChannels.call(this, "allowedChannels");
-        AbstractEventSecurityConstraint.getChannels.call(this, "ignoredChannels");
+        this.getChannels("allowedChannels");
+        this.getChannels("ignoredChannels");
 
-        AbstractEventSecurityConstraint.getRoles.call(this, "allowedRoles");
-        AbstractEventSecurityConstraint.getRoles.call(this, "ignoredRoles");
+        this.getRoles("allowedRoles");
+        this.getRoles("ignoredRoles");
     }
 
     /**
@@ -69,11 +69,11 @@ export abstract class AbstractEventSecurityConstraint extends AbstractModel impl
      */
     @BeforeInsert()
     private marshallTransformer(): void {
-        AbstractEventSecurityConstraint.setChannels.call(this, "allowedChannels");
-        AbstractEventSecurityConstraint.setChannels.call(this, "ignoredChannels");
+        this.setChannels(this.allowedChannels, "allowedChannels");
+        this.setChannels(this.ignoredChannels, "ignoredChannels");
 
-        AbstractEventSecurityConstraint.setRoles.call(this, "allowedRoles");
-        AbstractEventSecurityConstraint.setRoles.call(this, "ignoredRoles");
+        this.setRoles(this.allowedRoles, "allowedRoles");
+        this.setRoles(this.ignoredRoles, "ignoredRoles");
     }
 
     @Column({
