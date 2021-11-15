c876f6d59e27d0c1e008b096056714dc99907fe4
VictoriqueMoe <loli@victorique.moe>
2021-11-15 21:35:49 +0000

    more fixes

--- src/model/DB/AbstractEventSecurityConstraint.ts
+++ src/model/DB/AbstractEventSecurityConstraint.ts
@@ -1,20 +1,12 @@
 import {IEventSecurityConstraint} from "./IEventSecurityConstraint";
 import {AbstractModel} from "./AbstractModel";
-import {AfterLoad, BeforeInsert, Column} from "typeorm";
+import {AfterLoad, Column} from "typeorm";
 import {Guild, GuildChannel, Role} from "discord.js";
 import {ArrayUtils} from "../../utils/Utils";
 import {container} from "tsyringe";
 import {Client} from "discordx";
 
 export abstract class AbstractEventSecurityConstraint extends AbstractModel implements IEventSecurityConstraint {
-
-    private setRoles(roles: Role[], prop: string): void {
-        if (!ArrayUtils.isValidArray(roles)) {
-            this[prop] = null;
-            return;
-        }
-        this[prop] = roles.map(role => role.id);
-    }
 
     private getRoles(prop: string): void {
         const value: string | null = this[prop];
@@ -36,14 +28,6 @@ export abstract class AbstractEventSecurityConstraint extends AbstractModel impl
         this[prop] = value.map(channelId => guild.channels.cache.get(channelId));
     }
 
-    private setChannels(channels: GuildChannel[], prop: string): void {
-        if (!ArrayUtils.isValidArray(channels)) {
-            this[prop] = null;
-            return;
-        }
-        this[prop] = channels.map(channel => channel.id);
-    }
-
     private getGuild(): Guild {
         const client = container.resolve(Client);
         return client.guilds.cache.get(this.guildId);
@@ -60,18 +44,6 @@ export abstract class AbstractEventSecurityConstraint extends AbstractModel impl
 
         this.getRoles("allowedRoles");
         this.getRoles("ignoredRoles");
-    }
-
-    /**
-     * Transform the values to String arrays to save into database
-     */
-    @BeforeInsert()
-    private marshallTransformer(): void {
-        this.setChannels(this.allowedChannels, "allowedChannels");
-        this.setChannels(this.ignoredChannels, "ignoredChannels");
-
-        this.setRoles(this.allowedRoles, "allowedRoles");
-        this.setRoles(this.ignoredRoles, "ignoredRoles");
     }
 
     @Column({
