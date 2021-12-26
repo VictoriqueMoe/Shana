0a78008ecdbe7c85ca2da823a5360e5619e0a700
VictoriqueMoe <loli@victorique.moe>
2021-12-26 22:58:43 +0000

    added all app commands from neatApplicationCommandSlash

--- src/model/guild/manager/CommandSecurityManager.ts
+++ src/model/guild/manager/CommandSecurityManager.ts
@@ -89,7 +89,7 @@ export class CommandSecurityManager extends BaseDAO<CommandSecurityModel> implem
 
 
     public getAllCommandNames(includeAlias: boolean = false): string[] {
-        const commands = [...this._metadata.simpleCommands, ...this._metadata.applicationCommands];
+        const commands = [...this._metadata.simpleCommands, ...this._metadata.neatApplicationCommandSlash];
         if (!includeAlias) {
             return commands.map(command => command.name);
         }
