9e7338fe0795d6ed9ceba19c5d4f52e1cb596ebd
VictoriqueMoe <loli@victorique.moe>
2022-03-28 14:34:11 +0100

    revert typeorm change until recursion is fixed and bump djx version

--- src/model/decorators/AutoModFilter.ts
+++
@@ -1,9 +0,0 @@
-import {AbstractFilter} from "../closeableModules/subModules/dynoAutoMod/AbstractFilter";
-import constructor from "tsyringe/dist/typings/types/constructor";
-import {container, injectable} from "tsyringe";
-import {Beans} from "../../DI/Beans";
-
-export function AutoModFilter(constructor: constructor<AbstractFilter>): void {
-    injectable()(constructor);
-    container.registerSingleton(Beans.ISubModuleToken, constructor);
-}
