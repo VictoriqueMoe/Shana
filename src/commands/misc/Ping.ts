622e49f7a51fb2d2d2ee6176c4c0439864c41de4
VictoriqueMoe <loli@victorique.moe>
2022-09-18 23:57:01 +0100

    improved message

--- src/commands/misc/Ping.ts
+++ src/commands/misc/Ping.ts
@@ -1,6 +1,5 @@
 import {Category} from "@discordx/utilities";
-import {oneLine} from "common-tags";
-import {CommandInteraction, PermissionsBitField} from "discord.js";
+import {CommandInteraction, EmbedBuilder, GuildMember, PermissionsBitField, Status} from "discord.js";
 import {Client, Discord, Slash} from "discordx";
 
 @Discord()
@@ -12,13 +11,41 @@ export class Ping {
         defaultMemberPermissions: PermissionsBitField.Flags.Administrator
     })
     public async ping(interaction: CommandInteraction, client: Client): Promise<void> {
-        const msg = (await interaction.reply({content: "Pinging...", fetchReply: true}));
-        const content = oneLine`
-          ${msg.inGuild() ? `${interaction.member},` : ""}
-          Pong! The message round-trip took
-          ${msg.createdTimestamp - interaction.createdTimestamp}ms.
-          ${client.ws.ping ? `The heartbeat ping is ${Math.round(client.ws.ping)}ms.` : ""}
-        `;
-        await msg.edit(content);
+        const msg = await interaction.reply({content: "Pinging...", fetchReply: true});
+
+        const messageTime = `${msg.createdTimestamp - interaction.createdTimestamp}ms`;
+        const heartBeat = `${Math.round(client.ws.ping)}ms`;
+        const websocketStatus = Status[client.ws.status];
+
+        const me = interaction?.guild?.members?.me ?? interaction.user;
+        const colour = me instanceof GuildMember ? me.displayHexColor : "#0099ff";
+        const embed = new EmbedBuilder()
+            .setTitle(`Ping information`)
+            .setColor(colour)
+            .setAuthor({
+                name: client.user.username,
+                iconURL: me.displayAvatarURL()
+            })
+            .setDescription(`Ping info from this bot and current status`)
+            .setTimestamp();
+
+        embed.addFields([
+            {
+                name: "message round-trip",
+                value: messageTime
+            },
+            {
+                name: "heartbeat ping",
+                value: heartBeat
+            },
+            {
+                name: "Websocket status",
+                value: websocketStatus
+            }
+        ]);
+        await msg.edit({
+            embeds: [embed],
+            content: `Pong!`
+        });
     }
 }
